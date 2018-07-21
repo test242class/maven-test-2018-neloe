c4e02c82426f8a9567b97b07bb3fd6217facd1f2
Nathan Eloe <powerofazure@gmail.com>
2018-07-21 11:18:54 -0500

    Initial failing test

---
+++ src/main/java/Foo.java
@@ -0,0 +1,5 @@
+public class Foo {
+    public static int bar() {
+        return -1;
+    }
+}
---
+++ src/test/java/FooTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+public class FooTest {
+
+    @org.junit.Test
+    public void bar() {
+        assertEquals(0, Foo.bar());
+    }
+}
