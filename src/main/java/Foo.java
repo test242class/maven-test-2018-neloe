e0323cb98d5347b2a53abb03647bfa31b39a2e41
Nathan Eloe <powerofazure@gmail.com>
2018-08-29 12:27:49 -0500

    Add required code header to source

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,3 +1,9 @@
+/*
+Name: Nathan Eloe
+Date: 8/29/2018
+Description: Demonstrate that the tests work as intended
+Academic honesty blurb
+ */
 public class Foo {
     public static int bar() {
         return -1;
